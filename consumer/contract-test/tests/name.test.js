d4120afe772dec7204cf8a3c36875c6e16609b1d
Dan Siwiec <daniel.siwiec@gmail.com>
2018-10-14 23:34:57 -0700

    Parametrize contract test

--- consumer/contract-test/tests/name.test.js
+++ consumer/contract-test/tests/name.test.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 
 describe('Name service', () => {
   it('should have the right schema', async () => {
-    const response = await axios('http://localhost:3000')
+    const response = await axios(process.env.endpoint)
     const body = response.data
     expect(body.name).toBeDefined()
     expect(body.name.first).toBeDefined()
